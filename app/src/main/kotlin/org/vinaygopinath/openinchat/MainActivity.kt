6b7038ce5638021d9c6287d6737381e35f1cb0a7
Vinay Gopinath <vinay@vinaygopinath.me>
2023-05-28 18:54:56 -0600

    Update UI and switch to activity
    This replaces the use of the Fragment with an updated UI in the Activity itself.

--- app/src/main/kotlin/org/vinaygopinath/openinchat/MainActivity.kt
+++ app/src/main/kotlin/org/vinaygopinath/openinchat/MainActivity.kt
@@ -5,9 +5,12 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import org.vinaygopinath.openinchat.fragment.MainFragment
+import org.vinaygopinath.openinchat.helpers.ClipboardHelper
 import org.vinaygopinath.openinchat.helpers.PhoneNumberHelper
 import javax.inject.Inject
 
@@ -17,12 +20,34 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var phoneNumberHelper: PhoneNumberHelper
 
+    @Inject
+    lateinit var clipboardHelper: ClipboardHelper
+
+    private lateinit var phoneNumberInput: EditText
+    private lateinit var messageInput: EditText
+    private lateinit var clipboardPasteButton: ImageButton
+    private lateinit var chooseContactButton: ImageButton
+    private lateinit var openInWhatsappButton: Button
+    private lateinit var openInSignalButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().add(R.id.container, MainFragment()).commit()
+        initializeView()
+    }
+
+    private fun initializeView() {
+        phoneNumberInput = findViewById(R.id.phone_number_input)
+        messageInput = findViewById(R.id.message_input)
+        findViewById<ImageButton>(R.id.paste_from_clipboard_button).setOnClickListener {
+            val content = clipboardHelper.readClipboardContent()
+            if (content is ClipboardHelper.ClipboardContent.ClipboardData) {
+                phoneNumberInput.setText(content.content)
+            }
         }
+        chooseContactButton = findViewById(R.id.choose_from_contacts_button)
+        openInWhatsappButton = findViewById(R.id.open_whatsapp_button)
+        openInSignalButton = findViewById(R.id.open_signal_button)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
