f487a209de52169b145e72429efd397470746c7e
Vinay Gopinath <vinay@vinaygopinath.me>
2023-05-31 00:11:13 -0600

    Add regex-based phone number parser
    This adds a new method to extract phone number(s) from the entered phone number input field using regex.
    The helper method is able to detect multiple phone numbers in the given text. A future commit will handle prompting the user to choose one of the detected phone numbers.

--- app/src/main/kotlin/org/vinaygopinath/openinchat/helpers/PhoneNumberHelper.kt
+++ app/src/main/kotlin/org/vinaygopinath/openinchat/helpers/PhoneNumberHelper.kt
@@ -5,8 +5,25 @@ import javax.inject.Inject
 
 class PhoneNumberHelper @Inject constructor(private val phoneNumberUtil: PhoneNumberUtil) {
 
+    private val phoneNumberRegex by lazy {
+        Regex("(?:tel:)?(\\+?[\\d- ()]+)")
+    }
+
+    private val invalidPhoneNumberCharactersRegex by lazy {
+        Regex("[-() ]*")
+    }
+
     fun isPhoneNumberValid(phoneNumberString: String, regionCode: String): Boolean {
         val phoneNumber = phoneNumberUtil.parse(phoneNumberString, regionCode)
         return phoneNumberUtil.isValidNumber(phoneNumber)
+    }
+
+    fun extractPhoneNumber(rawString: String): List<String> {
+        val matches = phoneNumberRegex.findAll(rawString)
+        return matches.filter { matchResult -> matchResult.groupValues.size == 2 }
+            .map { matchResult -> matchResult.groupValues[1] }
+            .filter { match -> match.isNotBlank() }
+            .map { match -> match.replace(invalidPhoneNumberCharactersRegex, "") }
+            .toList()
     }
 }
