9e86ae0fa76bd70b6575439fe32fd4534b5b93b2
Vinay Gopinath <vinay@vinaygopinath.me>
2024-10-27 01:40:56 -0600

    Refactor Dao injection into new DatabaseDaoModule
    The new DatabaseDaoModule can be used as-is in both the source and tests, while the DatabaseModule is replaced by an in-memory database for unit tests.

--- app/src/test/kotlin/org/vinaygopinath/launchchat/di/TestDatabaseModule.kt
+++ app/src/test/kotlin/org/vinaygopinath/launchchat/di/TestDatabaseModule.kt
@@ -1,21 +1,27 @@
 package org.vinaygopinath.launchchat.di
 
 import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 import org.vinaygopinath.launchchat.AppDatabase
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
-internal object DatabaseModule {
+@TestInstallIn(
+    replaces = [DatabaseModule::class],
+    components = [SingletonComponent::class]
+)
+class TestDatabaseModule {
 
     @Provides
     @Singleton
     fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
-        return AppDatabase.buildDatabase(context)
+        return Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
     }
 }
