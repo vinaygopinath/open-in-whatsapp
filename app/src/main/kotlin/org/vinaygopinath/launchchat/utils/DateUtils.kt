d740d7785488c9efe97776867af009ea2c651279
Vinay Gopinath <vinay@vinaygopinath.me>
2024-10-27 01:36:08 -0600

    Update Instant creation to use millisecond precision
    This adds a new Instant extension function that sets the precision of DateUtils-generated Instants to use millisecond precision (and not microsecond precision, as is the default).
    The Instant extension addresses the problem when a test creates an Instant (with microsecond precision) which is then persisted into a SQLite database (which uses millisecond precision) and then compared against the original record, the timestamp would be flagged as not matching due to the precision mismatch.

--- app/src/main/kotlin/org/vinaygopinath/launchchat/utils/DateUtils.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/utils/DateUtils.kt
@@ -1,6 +1,9 @@
 package org.vinaygopinath.launchchat.utils
 
+import org.vinaygopinath.launchchat.extensions.withMillisecondPrecision
 import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.time.temporal.ChronoField
 import javax.inject.Inject
 
@@ -9,6 +12,11 @@ class DateUtils @Inject constructor(
 ) {
 
     fun getCurrentInstant(): Instant {
-        return clockProvider.getClock().instant().with(ChronoField.MILLI_OF_SECOND, 0)
+        return clockProvider.getClock().instant().withMillisecondPrecision()
+    }
+
+    fun getDateTimeString(instant: Instant, formatStyle: FormatStyle = FormatStyle.SHORT): String {
+        return DateTimeFormatter.ofLocalizedDateTime(formatStyle)
+            .format(instant.atZone(clockProvider.getClock().zone))
     }
 }
--- app/src/testFixtures/kotlin/factories/ActivityFactory.kt
+++ app/src/testFixtures/kotlin/factories/ActivityFactory.kt
@@ -1,5 +1,6 @@
 package factories
 
+import org.vinaygopinath.launchchat.extensions.withMillisecondPrecision
 import org.vinaygopinath.launchchat.models.Activity
 import org.vinaygopinath.launchchat.models.Activity.Source
 import java.time.Instant
@@ -7,11 +8,11 @@ import java.time.Instant
 object ActivityFactory {
 
     fun build(
-        id: Long = 1,
+        id: Long = 0,
         content: String = "some-content",
         source: Source = Source.TEXT_SHARE,
         message: String? = null,
-        occurredAt: Instant = Instant.now()
+        occurredAt: Instant = Instant.now().withMillisecondPrecision()
     ): Activity {
         return Activity(
             id = id,
